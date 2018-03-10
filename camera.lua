6eed5cb55f6b66d76fc72507c1a70d1cb256f61f
Jake <atone.sb@gmail.com>
2018-03-10 01:30:56 -0500

    housekeeping

--- camera.lua
+++ camera.lua
@@ -1,16 +1,16 @@
-local Camera = {}
+local Camera = {} --Camera effect following player.
 
-local function lerp(a, b, t) -- Linear interpolation function, returning a value some percent t of the way between a and b.
+local function lerp(a, b, t) -- Linear interpolation function, returning a value some percent t (0.01 = 1%) of the way between a and b.
   return a * (1-t) + (b*t)
 end
 
-local function draw(self)
+local function draw(self) --Offsets subsequent draws
   love.graphics.origin()
   love.graphics.translate(self.x, self.y)
 end
 
 local function update(self)
-  self.x = lerp( self.x or (self.game.middle.x - self.game.player.x) * w, (self.game.middle.x - self.game.player.x) * w, self.speed) --Lerp between current offset and new offset for smooth camera effect.
+  self.x = lerp( self.x or (self.game.middle.x - self.game.player.x) * w, (self.game.middle.x - self.game.player.x) * w, self.speed) --Lerp between current offset and new offset for smooth camera.
   self.y = lerp( self.y or (self.game.middle.y - self.game.player.y) * w, (self.game.middle.y - self.game.player.y) * w, self.speed)
 end
 
